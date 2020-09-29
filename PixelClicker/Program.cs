31ed14a7d2585716717492064afe5d3d4d9d468f
Bennett G Sewell <bennettgsewell@gmail.com>
2020-09-29 14:52:50 -0400

    Created general to do list in Program.cs

--- PixelClicker/Program.cs
+++ PixelClicker/Program.cs
@@ -10,6 +10,16 @@ namespace PixelClicker
     {
         static void Main(string[] args)
         {
+            //TODO: Grab portions of the screen, remove mouse pointer.
+            //TODO: Serialize portions of the screen as files.
+            //TODO: Compare that porions of the screen match serialized files.
+
+            //TODO: Function to move mouse.
+            //TODO: Function click screen.
+
+            //Add Python scripting functionality.
+
+            //Create script to test the BOYUM addon.
         }
     }
 }
