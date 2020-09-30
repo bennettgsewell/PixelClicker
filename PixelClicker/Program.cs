2089eddac6e8c91ffb4106c9903189f07aea6860
Bennett G Sewell <bennettgsewell@gmail.com>
2020-09-30 15:44:30 -0400

    Added command line option to take screenshot.

--- PixelClicker/Program.cs
+++ PixelClicker/Program.cs
@@ -4,6 +4,7 @@ using NDesk.Options;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting;
 using System.Text;
@@ -16,20 +17,78 @@ namespace PixelClicker
 {
     class Program
     {
+        /// <summary>
+        /// The tasks the program can perform.
+        /// </summary>
+        enum Task
+        {
+            None,
+            DisplayMousePos,
+            PythonConsole,
+            Screenshot,
+        }
+
         static void Main(string[] args)
         {
-            //TODO: Serialize portions of the screen as files.
-            //TODO: Compare that porions of the screen match serialized files.
+            //The task the user is going to execute.
+            Task task = Task.None;
 
-            //Add Python scripting functionality.
+            //Selected region of the screen.
+            Rectangle region = default;
+            
+            //Output file location.
+            string file = null;
 
-            //Create script to test the BOYUM addon.
+            //Parse command line arguments.
             OptionSet os = new OptionSet()
             {
-                { "m|mouse", "Displays the current mouse posiiton on the screen.", v => DisplayCurrentMousePosition() },
-                { "c|console", "Opens a Python console to execute commands.", v => PythonConsole() },
+                { "m|mouse", "Displays the current mouse posiiton on the screen.", v => task = Task.DisplayMousePos },
+                { "c|console", "Opens a Python console to execute commands.", v => task = Task.PythonConsole },
+                { "s|screenshot=", "Takes a screenshot and saves it to a .ss file. s=x,y,width,height", v =>
+                    {
+                        task = Task.Screenshot;
+                        string[] parts = v.Split(',');
+                        if(parts.Length != 4)
+                            throw new ArgumentException("-s incorrectly formatted, expected \"-s=x,y,width,height\"");
+                        region = new Rectangle(int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2]), int.Parse(parts[3]));
+                    }
+                },
+                { "f|file=", "The output file path", v =>
+                    {
+                        if(Uri.IsWellFormedUriString(v, UriKind.RelativeOrAbsolute))
+                            file = v;
+                        else
+                            throw new ArgumentException("-file is incorrectly formatted.");
+                    }
+                },
             };
-            os.Parse(args);
+            try
+            {
+                os.Parse(args);
+            }
+            catch (ArgumentException ar)
+            {
+                Console.Error.WriteLine(ar.Message);
+            }
+
+            switch (task)
+            {
+                case Task.DisplayMousePos:
+                    DisplayCurrentMousePosition();
+                    break;
+                case Task.PythonConsole:
+                    PythonConsole();
+                    break;
+                case Task.Screenshot:
+                    if(file == null)
+                    {
+                        Console.Error.WriteLine("-file=path_to_file must be set to take a screenshot.");
+                        return;
+                    }
+                    using(Screenshot ss = new Screenshot(region))
+                        ss.SaveToFile(file);
+                    break;
+            }
         }
 
         /// <summary>
@@ -39,7 +98,7 @@ namespace PixelClicker
         {
             ScriptEngine py = Python.CreateEngine();
             ScriptScope scope = py.CreateScope();
-            
+
             bool consoleRunning = true;
 
             //Add quit command
