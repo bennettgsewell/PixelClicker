ea5bd37c1fec8afce806a7b727f34bb5d426b37e
Bennett G Sewell <bennettgsewell@gmail.com>
2020-09-30 15:28:07 -0400

    Added a Python console to the program.

--- PixelClicker/Program.cs
+++ PixelClicker/Program.cs
@@ -1,8 +1,11 @@
-﻿using NDesk.Options;
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using NDesk.Options;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -23,9 +26,49 @@ namespace PixelClicker
             //Create script to test the BOYUM addon.
             OptionSet os = new OptionSet()
             {
-                { "m|mouse", "Displays the current mouse posiiton on the screen.", v => DisplayCurrentMousePosition() }
+                { "m|mouse", "Displays the current mouse posiiton on the screen.", v => DisplayCurrentMousePosition() },
+                { "c|console", "Opens a Python console to execute commands.", v => PythonConsole() },
             };
             os.Parse(args);
+        }
+
+        /// <summary>
+        /// Displays a Python console to the user
+        /// </summary>
+        private static void PythonConsole()
+        {
+            ScriptEngine py = Python.CreateEngine();
+            ScriptScope scope = py.CreateScope();
+            
+            bool consoleRunning = true;
+
+            //Add quit command
+            Action quit = new Action(() => consoleRunning = false);
+            scope.SetVariable("exit", quit);
+            scope.SetVariable("quit", quit);
+
+            while (consoleRunning)
+            {
+                try
+                {
+                    //Read command string from user.
+                    if (Console.CursorLeft != 0)//Before printing >>> make sure it's on a new line.
+                        Console.WriteLine();
+                    Console.Write(">>> ");
+                    string input = Console.ReadLine();
+
+                    //Execute the command.
+                    ObjectHandle objHand = py.ExecuteAndWrap(input, scope);
+
+                    //See if there's a result.
+                    object result = objHand.Unwrap();
+                    Console.Write(result == null ? string.Empty : result.ToString());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
 
         /// <summary>
